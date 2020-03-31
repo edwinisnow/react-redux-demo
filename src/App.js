bb12862c69cd7ee1de1b184b0a4eecdc61d07fa0
edwinisnow <edwin04@gmail.com>
2020-03-31 02:21:49 +0100

    Store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
+import store from './redux/store'
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
+
   );
 }
 
